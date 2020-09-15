d6e163b9774f989b02260f6573f6c1924b3ae2c9
Kadence Neuens <kadenceneuens@gmail.com>
2020-09-15 15:21:26 -0500

    Added eslint exception for Buddy to deploy properly

--- src/Components/Projects/image-link-slider.jsx
+++ src/Components/Projects/image-link-slider.jsx
@@ -6,6 +6,7 @@ export default function ImageLinkSlider(props)
     return(
         <>
             <h2>Image Link Slider</h2>
+            {/* eslint-disable-next-line */}
             <img src={ImageLinkSliderPreview} alt="Preview animation for Image Link Slider"/>
             <p>A simple image link slider component built for React. Uses react-router-dom for 
                 links, react-use-gesture for drag gesture scrolling, and react-spring for animation. 
